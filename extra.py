0406cadefe9e154bc15bb6557ee56b41d5b9ac53
sachi3050 <122316201+sachi3050@users.noreply.github.com>
2026-02-09 20:31:01 +0530

    Create extra.py

---
+++ extra.py
@@ -0,0 +1,11 @@
+#InstalL Python and Pip (If you haven't already) 
+sudo apt update
+sudo apt install -y python3 python3-pip python3-venv
+
+#Creating a Virtual Env (Optional But Recommended)
+sudo apt install -y python3 python3-pip python3-venv
+
+python3 -m venv venv
+source venv/bin/activate
+
+
